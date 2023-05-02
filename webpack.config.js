8c4810b7d841c243a1802b4422cdc3af668d11ff
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-05-02 17:39:17 +0530

    fix config issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-    },
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            template: './src/index.html',
-        }),
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    mode: 'none',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  mode: 'none',
 };
